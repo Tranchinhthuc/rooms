ac0d614bd8252166573a845c585b979fec5c9c1f
Hoang Xuan Thanh <thanh.hx@zinza.com.vn>
2018-08-05 21:40:05 +0700

    Remove devise employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 4.times do |i|
-  Employee.create(name: "Employee_#{i +1 }", email: "employee_#{i}@example.com", password: 'password')
+  Employee.create(name: "Employee_#{i +1 }", email: "employee_#{i}@example.com")
 end
